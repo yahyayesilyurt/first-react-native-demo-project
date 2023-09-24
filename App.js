c51bcc21d188eaa2b842951462e006ebee71dfa5
yahyayesilyurt <yesilyurty21@itu.edu.tr>
2023-09-24 12:12:27 +0300

    improved layout

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your goal!" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -16,11 +16,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
@@ -28,5 +35,8 @@ const styles = StyleSheet.create({
     width: "80%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
